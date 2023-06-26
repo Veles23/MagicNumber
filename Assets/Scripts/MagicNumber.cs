82804622adced67671be471ec5f86d5fa64967b8
Andrey Alferov <alukard017@gmail.com>
2023-06-26 22:56:50 +0300

    HomeWork1-1

--- Assets/Scripts/MagicNumber.cs
+++ Assets/Scripts/MagicNumber.cs
@@ -1,52 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MagicsNumb : MonoBehaviour
 {
-    private int _min;
-    private int _max;
-    private int _guess;
+    #region Variables
 
-    void Start()
+    public TMP_Text firsText;
+    public int max;
+    public int min;
+    public TMP_Text otherText;
+    private int _guess;
+    private int _numberOfHalls;
+
+    #endregion
+
+    #region Unity lifecycle
+
+    private void Start()
     {
-        _min = 1;
-        _max = 1000;
-        Debug.Log($"Привет! Загадай число от {_min} до {_max}!");
+        min = 1;
+        max = 1000;
+        firsText.text = $"Загадай число от {min} до {max}!\n\n" +
+                        "Твое число больше? Жми стрелку вверх." +
+                        "\nТвоё число меньше? Жми стрелку вниз." +
+                        "\nЯ угадал твое число? Жми пробел." +
+                        "\n\nДля старта нажми стрелку вверх или вниз.";
+
         CalculateGuess();
         AskGuess();
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            _max = _guess;
+            _numberOfHalls++;
+            max = _guess;
             CalculateGuess();
             AskGuess();
         }
-
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            _min = _guess;
+            _numberOfHalls++;
+            min = _guess;
             CalculateGuess();
             AskGuess();
-            
         }
         else if (Input.GetKeyDown(KeyCode.Space))
-
         {
-            Debug.Log($"Поздравляю! Твоё число {_guess}!");
+            otherText.text = $"Хе-хе! Твоё число {_guess}! Колличество ходов: {_numberOfHalls}.";
         }
     }
 
+    #endregion
+
+    #region Private methods
+
     private void AskGuess()
     {
-        Debug.Log($"Твоё число {_guess}?");
+        otherText.text = $"Твоё число {_guess}?";
     }
 
     private void CalculateGuess()
     {
-        _guess = (_min + _max) / 2;
+        _guess = (min + max) / 2;
     }
+
+    #endregion
 }
---
+++ Assets/Scripts/PushTheNumber.cs
@@ -0,0 +1,103 @@
+using TMPro;
+using UnityEngine;
+
+public class PushTheNum : MonoBehaviour
+{
+    #region Variables
+
+    public TMP_Text myText;
+    private int _step;
+    private int _sum;
+
+    #endregion
+
+    #region Unity lifecycle
+
+    private void Start()
+    {
+        Debug.Log("Нажмите цифру!");
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SumNumbers();
+            Debug.Log("Сумма: " + _sum);
+        }
+
+        else if (_sum == 50)
+        {
+            Debug.Log($"Сумма {_sum}! Игра окончена. Колличество ходов {_step}");
+        }
+
+        else if (_sum > 50)
+        {
+            EndSum();
+            Debug.Log($"Сумма {_sum}! Игра окончена. Колличество ходов {_step}");
+        }
+    }
+
+    #endregion
+
+    #region Private methods
+
+    private int EndSum()
+    {
+        int endSum = _sum - 50;
+        _sum -= endSum;
+        return _sum;
+    }
+
+    private void SumNumbers()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _sum++;
+            _step++;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _sum += 2;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _sum += 3;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            _sum += 4;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            _sum += 5;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            _sum += 6;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            _sum += 7;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            _sum += 8;
+            _step++;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            _sum += 9;
+            _step++;
+        }
+    }
+
+    #endregion
+}
